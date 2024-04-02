a8a2089b0ccfe3123ea415ecd1c6e9cb054fefa8
Rafe Uddaraj <78890044+rafeuddaraj@users.noreply.github.com>
2024-04-02 13:22:46 +0600

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <div className="bkash-header"></div>
+      </div>
+    </main>
+  );
+}
